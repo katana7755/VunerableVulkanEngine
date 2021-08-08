cc2f292f578c6f09472a7243ac04e8a60adef176
katana7755 <jaeho@unity3d.com>
2021-08-08 20:18:41 +0900

    converted IMGUI's secondary command buffer from reusable to disposable.

--- VunerableVulkanEngine/VunerableVulkanEngine.cpp
+++ VunerableVulkanEngine/VunerableVulkanEngine.cpp
@@ -325,7 +325,7 @@ void DrawImGuiFrame()
         auto command_buffer = VulkanGraphicsResourceCommandBufferManager::AllocateAdditionalCommandBuffer();
         VkCommandBufferBeginInfo begin_info = {};
         begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-        begin_info.flags |= (VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT);
+        begin_info.flags |= VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
         auto err = vkBeginCommandBuffer(command_buffer, &begin_info);
         check_vk_result(err);
